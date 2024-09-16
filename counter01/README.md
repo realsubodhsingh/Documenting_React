f58372ea9ccf0ef6668a7cbb1710edc5aa5fede7
realsubodh <subodhsingh3477@gmail.com>
2024-09-17 04:13:22 +0530

    hooks

--- counter01/README.md
+++ counter01/README.md
@@ -6,19 +6,3 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
-
-
-
-## How the file works together
-### How They Work Together:
-- main.jsx: Loads the app into the HTML. <br>
-- App.jsx: Defines the UI components. <br>
-- index.css: Applies global styles. <br>
-- App.css: Styles specific to the App.jsx component.
-<br>
-In short:
-
-App.jsx: UI logic. <br>
-main.jsx: The entry point, linking React to the DOM.<br>
-index.css: Global styles.<br>
-App.css: Component-specific styles for App.jsx.
---
+++ counter01/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+// in this we are gonna to learn about hooks.
+
+function App() {
+
+// let counter = 1443       .. conventional method
+
+let [counter, setCounter] = useState(0)   // hook
+
+// below arrow funtion is use to adding value in the button and counter funtion
+const addValue = () =>{
+  console.log("clicked", counter)
+  setCounter(counter + 1)
+}
+
+// now this arrow function is gonna to reduce the value from the point where the current value is:
+const removeValue = ()=>{
+console.log("clicked", counter)
+counter = counter - 1
+setCounter(counter)
+}
+  return (
+    <>
+      <h1>chaii aur react</h1>
+      <h2>counter value: {counter}</h2>
+      <button
+      onClick={addValue}>Add value: {counter}</button>
+      <br />
+      <button
+      onClick={removeValue}>remove value {counter}</button>
+    </>
+  )
+}
+
+export default App
