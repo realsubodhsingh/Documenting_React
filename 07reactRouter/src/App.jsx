7172a2e81ec1f8f3bc2215d98bd4d5c739bc7a67
realsubodh <subodhsingh3477@gmail.com>
2024-10-01 04:12:43 +0530

    learning react router

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./components/Header/header";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return (
-    <>
-     
-    </>
-  )
+  return <></>;
 }
 
-export default App
+export default App;
--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -1,10 +1,53 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Layout from "./Layout.jsx";
+import Home from "./components/Home/Home.jsx";
+import About from "./components/About/About.jsx";
+import Contact from "./components/Contact/Contact.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// this is the first way to create router in reactjs
+
+// const router = createBrowserRouter([
+//   {
+//     path:'/',
+//     element:<Layout/>,
+//     children:[
+//       {
+//         path:"",
+//         element:<Home/>
+//       },
+//       {
+//         path:"about",
+//         element:<About/>
+//       },
+//       {
+//         path:"contact",
+//         element:<Contact/>
+//       }
+//     ]
+//   }
+// ])
+
+// this is the second way of creating router in react js not much easy than the first one but you can go with it for better understanding of the code
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+    </Route>
+  )
+);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
